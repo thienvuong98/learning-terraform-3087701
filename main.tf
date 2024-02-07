bde6285e350845865314f5dd290ceb102ed60788
thienv <thienv@gmail.com>
2024-02-07 14:19:46 -0800

    Fix syntax error

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_instance" "blog" {
 module "blog_sg" {
   source              = "terraform-aws-modules/security-group/aws"
   version             = "5.1.0"
-  name                = "blog_new"
+  name                = "blog"
   vpc_id              = data.aws_vpc.default.id
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
