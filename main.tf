1c9bb8d1bb6eb68642cf6afe39183e0e540fd8f0
thienvuong98 <136645171+thienvuong98@users.noreply.github.com>
2024-02-06 21:56:06 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "aws_ami" "app_ami" {
 }
 
 
-resource "aws_instance" "web" {
+resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
