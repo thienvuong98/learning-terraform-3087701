84f98c9d0eaae6c346b42e4557f62cdcae997407
thienv <thienv@gmail.com>
2024-02-07 15:58:07 -0800

    Fix syntax error

--- main.tf
+++ main.tf
@@ -70,10 +70,10 @@ module "alb" {
   target_groups = [
     {
       name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
-      targets = {
+      targets          = {
         my_target = {
           target_id = aws_instance.blog.id
           port      = 80
