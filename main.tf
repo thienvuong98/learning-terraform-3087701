dac74d422f97d8096ea4b32c612b3c13aa5900b4
thienv <thienv@gmail.com>
2024-02-07 16:01:08 -0800

    Fix syntax error

--- main.tf
+++ main.tf
@@ -57,17 +57,17 @@ module "blog_sg" {
 }
 
 module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
+  source             = "terraform-aws-modules/alb/aws"
+  version            = "~> 6.0"
+  name               = "blog-alb"
 
-  name    = "blog-alb"
   load_balancer_type = "application"
 
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
 
-  target_groups = [
+  target_groups      = [
     {
       name_prefix      = "blog-"
       backend_protocol = "HTTP"
