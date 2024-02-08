a9830f192dccbfb2dfe343101a275335bda24dab
thienv <thienv@gmail.com>
2024-02-07 17:39:06 -0800

    Fix syntax error

--- main.tf
+++ main.tf
@@ -33,16 +33,19 @@ module "blog_vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+module "autoscaling" {
+  source   = "terraform-aws-modules/autoscaling/aws"
+  version  = "7.3.1"
+  name     = "blog"
+  min_size = 1
+  max_size = 2
 
-  subnet_id = module.blog_vpc.public_subnets[0]
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns   = module.blog_alb.target_group_arns
+  security_groups     = [module.blog_sg.security_group_id]
 
-  tags = {
-    Name = "Learning Terraform"
-  }
+  image_id            = data.aws_ami.app_ami.id
+  instance_type       = var.instance_type
 }
 
 module "blog_sg" {
@@ -56,7 +59,7 @@ module "blog_sg" {
   egress_cidr_blocks  = ["0.0.0.0/0"]
 }
 
-module "alb" {
+module "blog_alb" {
   source             = "terraform-aws-modules/alb/aws"
   version            = "~> 6.0"
   name               = "blog-alb"
